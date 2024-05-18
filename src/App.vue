ded2ff417f763182b929e7aa392b67948204ca35
소희 <105264974+sohee-commits@users.noreply.github.com>
2024-05-18 16:52:19 +0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import WeatherApp from './components/icons/WeatherApp.vue';
+export default {
+	components: {
+		WeatherApp,
+	},
+};
+</script>
+
+<template>
+	<weather-app></weather-app>
+</template>
+
+<style scoped></style>
